0b07ddf860c4ea9dc1fb869a99478cbb11f1981b
EstebanAtHisComputer <estnicolas@proton.me>
2026-01-14 18:08:43 +0100

    feat: Display version number on the intro page

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,16 +1,40 @@
 import 'package:file_selector/file_selector.dart';
 import 'package:flutter/material.dart';
 import 'package:kobo_highlights/db.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 import './main_page.dart';
 import '../utils.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
+  @override
+  State<StatefulWidget> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  String version = "VER_NOT_FOUND";
+
+  _fetchVersion() async {
+    PackageInfo packageInfo = await PackageInfo.fromPlatform();
+    String appName = packageInfo.appName;
+    String appVersion = packageInfo.version;
+    setState(() {
+      version = "$appName $appVersion";
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchVersion();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -49,9 +73,12 @@ class IntroPage extends StatelessWidget {
               ),
               child: const Text("Open"),
             ),
-            Padding(padding: EdgeInsetsGeometry.all(4.0)),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.transparent,
+        child: Opacity(opacity: 0.5, child: Text(version)),
       ),
     );
   }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import package_info_plus
 import screen_retriever_macos
 import window_manager_plus
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   WindowManagerPlusPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlusPlugin"))
 }
